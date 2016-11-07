2d160cf93fb03fa662ef3fd543e6fe21d77ed8a2
Amelia Brown <amelia.brown389@gmail.com>
2016-11-06 17:59:59 -0800

    (chore): add webpack config, react entry and index.html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let App = () => (
+  <div>Hello world</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('mount'));
--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,5 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
 module.exports = {
-  entry: " "
-  output:
-  loaders:
+  entry: path.resolve(__dirname, '..', 'src'),
+  output: {
+    path: path.resolve(__dirname, '..', 'lib'),
+    public_path: 'lib',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sass|css)$/,
+        loader: 'style!css!sass',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json$/,
+        loader:'json-loader',
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'file',
+        include: path.resolve(__dirname, 'client/images'),
+        exclude: /node_modules/,
+      }
+    ],
+  }
 }
